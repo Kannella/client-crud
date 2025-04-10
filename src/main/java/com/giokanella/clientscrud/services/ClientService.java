31b3026ce3a9853cee1fd07f42b600763ff4288b
Giovanni <gi.canella85@outlook.com>
2025-04-10 08:32:03 -0300

    Find By Id

---
+++ src/main/java/com/giokanella/clientscrud/services/ClientService.java
@@ -0,0 +1,24 @@
+package com.giokanella.clientscrud.services;
+
+import com.giokanella.clientscrud.dto.ClientDTO;
+import com.giokanella.clientscrud.entities.Client;
+import com.giokanella.clientscrud.repositories.ClientRepository;
+import com.giokanella.clientscrud.services.exceptions.ResourcesNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    //FindById
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).orElseThrow(() -> new ResourcesNotFoundException("Recurso não encontrado!"));
+        return new ClientDTO(client);
+    }
+
+}
