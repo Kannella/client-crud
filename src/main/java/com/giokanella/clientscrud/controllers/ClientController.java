b94661e6421882618b00856e783aefe85a4c0fab
Giovanni <gi.canella85@outlook.com>
2025-04-10 08:46:58 -0300

    Insert

--- src/main/java/com/giokanella/clientscrud/controllers/ClientController.java
+++ src/main/java/com/giokanella/clientscrud/controllers/ClientController.java
@@ -2,12 +2,13 @@ package com.giokanella.clientscrud.controllers;
 
 import com.giokanella.clientscrud.dto.ClientDTO;
 import com.giokanella.clientscrud.services.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -20,6 +21,13 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
